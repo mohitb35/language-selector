b82d872ae84739f639b4ebfd6e26bcd20323f9b3
Mohit Bajaj <mohit_b35@hotmail.com>
2020-06-16 11:31:56 +0530

    consuming values from Context (type 2) - using Context.Consumer instead of this.context.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
+import LanguageContext from '../contexts/languageContext';
+
+
 class Button extends React.Component {
+	// static contextType = LanguageContext;
+
 	render() {
+		console.log(LanguageContext);
+
+		// const text = this.context === 'english' ? 'Submit' : 'Voorleggen';
 		return (
-			<button className="ui button primary">Submit</button>
+			<button className="ui button primary">
+				{/* {text} */}
+				<LanguageContext.Consumer>
+					{(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
+				</LanguageContext.Consumer>
+			</button>
 		)
 	}
 }
